966a70f2a87155cf33e2f264d31f2bccfcb894c0
Eugene Kim <eugenekim.3.21@gmail.com>
2022-09-20 14:42:30 -0400

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,66 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:import/recommended',
+  ],
+  'settings': {
+    'import/extensions': [
+      '.js',
+      '.jsx'
+    ],
+    'import/resolver': {
+      'node': {
+        'paths': ['src'],
+        'extensions': ['.js', '.jsx', '.ts', '.tsx'],
+      }
+    },
+  },
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react',
+    'unused-imports'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2,
+      {
+        'SwitchCase': 1
+      }
+    ],
+    'no-unused-vars': ['warn', { 'vars': 'all', 'args': 'after-used', 'ignoreRestSiblings': false }],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 1 }],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      { 'vars': 'all', 'varsIgnorePattern': '^_', 'args': 'after-used', 'argsIgnorePattern': '^_' }
+    ],
+    'import/order': ['error', {
+      'groups': ['builtin', 'external', 'parent', 'sibling', 'index'],
+      'newlines-between': 'always'
+    }],
+    'react/prop-types': 'off'
+  }
+};
